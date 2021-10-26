9c6dcb9a0939c844fe8ed7601209833f5d852ef3
loveagile <happyplandry@gmail.com>
2021-10-26 10:10:06 +1000

    Update about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -6,12 +6,19 @@ function About() {
       <div className="custom-box-shadow my-5 p-4 rounded">
         <div className="question my-2">
           <h3>What is Sentiment Analysis</h3>
-          <p>
-            Sentiment analysis (or opinion mining) uses NLP to determine whether
+          <ul>
+            <li>Sentiment analysis (or opinion mining) uses NLP to determine whether
             data is positive, negative or neutral. We use sentiment analysis on
             cryptocurrency related tweets to help people make better investment
-            decisions.
-          </p>
+            decisions.</li>
+            <li className="my-2">
+             AFINN is a list of words rated for valence with an integer between minus five (negative) and plus five (positive). Sentiment analysis is performed by cross-checking the string tokens (words, emojis) with the AFINN list and getting their respective scores. 
+            </li>
+            <li>
+            While the accuracy provided by AFINN is quite good considering it's computational performance there is always room for improvement.
+            </li>
+          </ul>
+          
         </div>
 
         <div className="question my-4">
@@ -41,7 +48,11 @@ function About() {
         </div>
         <div className="question my-4">
           <h3>Interpreting Sentiment Analysis</h3>
-          <ul>{/* TODO: Update this section */}</ul>
+          <ul>
+            <li>If the score is <span className="fw-bold">greater than</span> 1.0 then the sentiment is positive</li>
+            <li>If the score is <span className="fw-bold">less than</span> 1.0 then the sentiment is negative</li>
+            <li>If the score is <span className="fw-bold">equal to</span> 1.0 then the sentiment is neutral</li>
+          </ul>
         </div>
       </div>
     </div>
